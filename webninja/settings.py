8c5b8a3bece3ca23888ddb0c97ca8b8046e1c0fb
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-05-02 14:44:58 +0600

    media urls added

--- webninja/settings.py
+++ webninja/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+# Add to the bottom of the file
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
